eb094d328bbf2677af109682c4a7954b3273b106
Brett Jurman <i.be.brett@gmail.com>
2019-06-27 20:03:22 -0400

    adding hello world boolean

---
+++ hello.v
@@ -0,0 +1,18 @@
+Inductive bool : Type :=
+  | true
+  | false.
+
+
+Definition opp (t: bool): bool :=
+  match t with 
+  | true => false
+  | false => true
+  end.
+
+
+Compute (opp false).
+Compute (opp true).
+
+Example test_opp:
+  opp true = false.
+Proof. simpl. reflexivity. Qed.
